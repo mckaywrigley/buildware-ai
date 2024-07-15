db1b03867a31ca5433e808606405c753d908d40c
Mckay Wrigley <mckaywrigley@gmail.com>
2024-07-15 15:38:52 -0600

    bump max tokens

--- lib/actions/llm/index.ts
+++ lib/actions/llm/index.ts
@@ -3,18 +3,25 @@
 import { LLMCost } from "@/lib/utils/llm-cost"
 import Anthropic from "@anthropic-ai/sdk"
 
-const anthropic = new Anthropic({})
+const anthropic = new Anthropic()
 
 export const generateAIResponse = async (
   messages: Anthropic.Messages.MessageParam[]
 ) => {
-  const message = await anthropic.messages.create({
-    model: "claude-3-5-sonnet-20240620",
-    system:
-      "You are a helpful assistant that can answer questions and help with tasks.",
-    messages,
-    max_tokens: parseInt(process.env.NEXT_PUBLIC_MAX_OUTPUT_TOKENS!)
-  })
+  const message = await anthropic.messages.create(
+    {
+      model: "claude-3-5-sonnet-20240620",
+      system:
+        "You are a helpful assistant that can answer questions and help with tasks.",
+      messages,
+      max_tokens: parseInt(process.env.NEXT_PUBLIC_MAX_OUTPUT_TOKENS!)
+    },
+    {
+      headers: {
+        "anthropic-beta": "max-tokens-3-5-sonnet-2024-07-15"
+      }
+    }
+  )
 
   // const { text, usage } = await generateText({
   //   model: anthropic("claude-3-5-sonnet-20240620"),
