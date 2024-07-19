fe3b65e8b528d4dfb718567e392b84c150cb8f41
Mckay Wrigley <mckaywrigley@gmail.com>
2024-07-19 07:59:27 -0600

    many updates

---
+++ actions/ai/improve-issue-prompt.ts
@@ -0,0 +1,178 @@
+"use server"
+
+import { calculateLLMCost } from "@/lib/ai/calculate-llm-cost"
+import { BUILDWARE_MAX_OUTPUT_TOKENS } from "@/lib/constants/buildware-config"
+import Anthropic from "@anthropic-ai/sdk"
+import endent from "endent"
+
+const anthropic = new Anthropic()
+
+export async function improveIssuePrompt(
+  startingIssue: {
+    name: string
+    content: string
+  },
+  messages: Anthropic.Messages.MessageParam[]
+): Promise<ParsedImproveIssueResponse> {
+  console.log("messages", messages)
+
+  const improvementModel = "claude-3-5-sonnet-20240620"
+
+  const systemPrompt = endent`
+    You are an expert project manager.
+
+    You will be given an issue and a set of improvement instructions.
+    
+    Your task is to improve the given issue.
+
+    To do this, you will chat back and forth with the user to create the best possible issue.
+
+    An ideal issue:
+    - Maintains the issue's original intent.
+    - Contains all necessary information and details.
+    - Lists specific requirements.
+    - Is clear and concise.
+    
+    Follow these steps for each message:
+    1. Carefully read the user's message.
+    2. Review the current issue.
+    3. Think about the best way to improve the issue.
+    4. Apply the improvements to the issue.
+    5. Repeat until the issue is done.
+  
+    ---
+
+    # Starting Issue
+
+    This is the starting issue as provided by the user:
+
+    <starting_issue_name>
+    ${startingIssue.name}
+    </starting_issue_name>
+
+    <starting_issue_content>
+    ${startingIssue.content}
+    </starting_issue_content>
+
+    ---
+
+    # Response Instructions
+
+    Follow these instructions for every response.
+
+    ## Response Information
+
+    Respond with the following information:
+    - RESPONSE: Enclose your response in <response> tags to help with parsing.
+      - IMPROVED_ISSUE: The improved issue.
+        - IMPROVED_ISSUE_NAME: The improved issue name.
+        - IMPROVED_ISSUE_CONTENT: The improved issue content.
+      - MESSAGE: Your message to the user.
+        - EXPLANATION: A brief explanation of the improvements made.
+        - NEXT_QUESTION: The next question to ask the user.
+      - IS_DONE: A boolean indicating if the issue is done. When this is true, the issue is complete and you should stop responding. This will trigger the issue to be created.
+
+    ## Response Format
+
+    Respond in the following format:
+    <response>
+      <improved_issue>
+        <improved_issue_name>__IMPROVED_ISSUE_NAME__</improved_issue_name>
+        <improved_issue_content>__IMPROVED_ISSUE_CONTENT__</improved_issue_content>
+      </improved_issue>
+
+      <message>
+        <explanation>__EXPLANATION_CONTENT__</explanation>
+
+        <next_question>__NEXT_QUESTION__</next_question>
+      </message>
+
+      <is_done>__IS_DONE__</is_done>
+    </response>
+    `
+
+  try {
+    const message = await anthropic.messages.create(
+      {
+        model: improvementModel,
+        system: systemPrompt,
+        messages,
+        max_tokens: BUILDWARE_MAX_OUTPUT_TOKENS
+      },
+      {
+        headers: {
+          "anthropic-beta": "max-tokens-3-5-sonnet-2024-07-15"
+        }
+      }
+    )
+
+    console.warn("improvement usage", message.usage)
+    const cost = calculateLLMCost({
+      llmId: improvementModel,
+      inputTokens: message.usage.input_tokens,
+      outputTokens: message.usage.output_tokens
+    })
+    console.warn("improvement cost", cost)
+
+    const parsedResponse = parseImproveIssuePrompt(
+      message.content[0].type === "text" ? message.content[0].text : ""
+    )
+
+    return parsedResponse
+  } catch (error) {
+    console.error("Error improving issue:", error)
+    throw error
+  }
+}
+
+interface ParsedImproveIssueResponse {
+  improvedIssue: {
+    name: string
+    content: string
+  }
+  message: {
+    explanation: string
+    nextQuestion: string
+  }
+  isDone: boolean
+}
+
+const parseImproveIssuePrompt = (
+  response: string
+): ParsedImproveIssueResponse => {
+  const improvedIssueMatch = response.match(
+    /<improved_issue>([\s\S]*?)<\/improved_issue>/
+  )
+  const improvedIssue = improvedIssueMatch ? improvedIssueMatch[1] : ""
+
+  const nameMatch = improvedIssue.match(
+    /<improved_issue_name>([\s\S]*?)<\/improved_issue_name>/
+  )
+  const contentMatch = improvedIssue.match(
+    /<improved_issue_content>([\s\S]*?)<\/improved_issue_content>/
+  )
+
+  const messageMatch = response.match(/<message>([\s\S]*?)<\/message>/)
+  const message = messageMatch ? messageMatch[1] : ""
+
+  const explanationMatch = message.match(
+    /<explanation>([\s\S]*?)<\/explanation>/
+  )
+  const nextQuestionMatch = message.match(
+    /<next_question>([\s\S]*?)<\/next_question>/
+  )
+
+  const isDoneMatch = response.match(/<is_done>(.*?)<\/is_done>/)
+
+  return {
+    improvedIssue: {
+      name: nameMatch ? nameMatch[1].trim() : "",
+      content: contentMatch ? contentMatch[1].trim() : ""
+    },
+    message: {
+      explanation: explanationMatch ? explanationMatch[1].trim() : "",
+      nextQuestion: nextQuestionMatch ? nextQuestionMatch[1].trim() : ""
+    },
+    isDone: isDoneMatch ? isDoneMatch[1].trim().toLowerCase() === "true" : false
+  }
+}
--- components/issues/chat-prompt-improver.tsx
+++ components/issues/chat-prompt-improver.tsx
@@ -1,54 +1,192 @@
 "use client"
 
-import { useChat } from "ai/react"
-import { useEffect } from "react"
+import { improveIssuePrompt } from "@/actions/ai/improve-issue-prompt"
+import Anthropic from "@anthropic-ai/sdk"
+import endent from "endent"
+import { Loader2, Send, Sparkles } from "lucide-react"
+import { FC, useEffect, useState } from "react"
+import ReactTextareaAutosize from "react-textarea-autosize"
+import { MessageMarkdown } from "../instructions/message-markdown"
+import { Button } from "../ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "../ui/dialog"
 
 interface ChatPromptImproverProps {
-  onMessagesUpdate: (messages: string[]) => void
+  startingIssue: {
+    name: string
+    content: string
+  }
 }
 
-export default function ChatPromptImprover({
-  onMessagesUpdate
-}: ChatPromptImproverProps) {
-  const { messages, input, handleInputChange, handleSubmit } = useChat()
+export const ChatPromptImprover: FC<ChatPromptImproverProps> = ({
+  startingIssue
+}) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const [isGenerating, setIsGenerating] = useState(false)
+  const [improvedIssue, setImprovedIssue] = useState(startingIssue)
+  const [userInput, setUserInput] = useState("")
+  const [messages, setMessages] = useState<Anthropic.Messages.MessageParam[]>(
+    []
+  )
 
   useEffect(() => {
-    onMessagesUpdate(messages.map(message => message.content))
-  }, [messages, onMessagesUpdate])
+    setImprovedIssue(startingIssue)
+  }, [startingIssue])
+
+  useEffect(() => {
+    if (isDialogOpen && messages.length === 0) {
+      handleSubmit("Please improve my issue.")
+    }
+  }, [isDialogOpen, messages])
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setUserInput(e.target.value)
+  }
+
+  const handleSubmit = async (message: string) => {
+    setUserInput("")
+    setIsGenerating(true)
+
+    const updatedMessages: Anthropic.Messages.MessageParam[] = [
+      ...messages,
+      { role: "user", content: message }
+    ]
+    setMessages(updatedMessages)
+
+    const messageResponse = await improveIssuePrompt(
+      startingIssue,
+      updatedMessages
+    )
+    console.log("messageResponse", messageResponse)
+    setImprovedIssue(messageResponse.improvedIssue)
+
+    const formattedResponse = messageResponse.isDone
+      ? "Completed!"
+      : endent`
+    ${messageResponse.message.explanation}
+
+    ${messageResponse.message.nextQuestion}
+    `
+    setMessages([
+      ...updatedMessages,
+      { role: "assistant", content: formattedResponse }
+    ])
+
+    setIsGenerating(false)
+  }
+
+  console.log("improvedIssue", improvedIssue)
 
   return (
-    <div className="p-4">
-      <div className="mb-4 space-y-2">
-        {messages.map(message => (
-          <div
-            key={message.id}
-            className={`rounded p-2 ${
-              message.role === "user"
-                ? "bg-gray-700 text-white"
-                : "bg-gray-500 text-white"
-            }`}
-          >
-            <strong>{message.role === "user" ? "User: " : "AI: "}</strong>
-            {message.content}
-          </div>
-        ))}
-      </div>
-
-      <form onSubmit={handleSubmit} className="flex gap-2">
-        <input
-          name="prompt"
-          value={input}
-          onChange={handleInputChange}
-          className="grow rounded border p-2"
-          placeholder="Type your message..."
-        />
-        <button
-          type="submit"
-          className="rounded bg-blue-500 px-4 py-2 text-white"
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger>
+        <Button
+          variant="outline"
+          disabled={isDialogOpen}
+          onClick={() => setIsDialogOpen(true)}
         >
-          Submit
-        </button>
-      </form>
-    </div>
+          <Sparkles className="mr-2 size-4" />
+          AI Improve
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent
+        className="flex max-h-[80vh] max-w-3xl flex-col gap-8 overflow-y-auto"
+        hideCloseButton={true}
+      >
+        <DialogHeader>
+          <DialogTitle>AI Issue Improver</DialogTitle>
+          <DialogDescription>
+            Chat with AI to improve your issue
+          </DialogDescription>
+        </DialogHeader>
+
+        <div>
+          {improvedIssue && (
+            <div className="h-[300px] overflow-y-auto rounded border p-2">
+              {isGenerating ? (
+                <div className="flex h-full items-center justify-center">
+                  <Loader2 className="mr-2 size-4 animate-spin" />
+                  Improving issue...
+                </div>
+              ) : (
+                <>
+                  <div className="text-sm font-bold">{improvedIssue.name}</div>
+                  <MessageMarkdown
+                    className="text-sm"
+                    content={improvedIssue.content}
+                  />
+                </>
+              )}
+            </div>
+          )}
+        </div>
+
+        <div className="mb-4 space-y-2">
+          {messages.map((message, index) => {
+            return (
+              <div key={index} className="">
+                <strong>{message.role === "user" ? "User: " : "AI: "}</strong>
+
+                {message.role === "assistant" ? (
+                  <MessageMarkdown
+                    className="text-sm"
+                    content={
+                      typeof message.content === "string"
+                        ? message.content
+                        : JSON.stringify(message.content)
+                    }
+                  />
+                ) : (
+                  <div className="text-sm">
+                    {typeof message.content === "string"
+                      ? message.content
+                      : JSON.stringify(message.content)}
+                  </div>
+                )}
+
+                <hr className="my-2" />
+              </div>
+            )
+          })}
+        </div>
+
+        <div className="flex gap-2">
+          <ReactTextareaAutosize
+            className="w-full resize-none rounded border bg-transparent px-3 py-2 focus-visible:outline-none"
+            placeholder="Type your message..."
+            minRows={2}
+            maxRows={5}
+            value={userInput}
+            onChange={handleInputChange}
+          />
+
+          <Button
+            className="rounded bg-blue-500 px-4 py-2 text-white"
+            onClick={() => handleSubmit(userInput)}
+            disabled={isGenerating}
+          >
+            <Send className="size-4" />
+          </Button>
+        </div>
+
+        <DialogFooter>
+          <Button
+            onClick={() => setIsGenerating(true)}
+            className="w-full max-w-[200px]"
+            disabled={isGenerating}
+          >
+            {isGenerating ? "Improving..." : "Create Issue"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
