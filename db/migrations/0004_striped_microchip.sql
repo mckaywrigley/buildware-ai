105ac49700c3ed6392475b481669d4d72ebc4b30
Mckay Wrigley <mckaywrigley@gmail.com>
2024-08-04 00:03:06 -0600

    simplify migration

--- db/migrations/0004_striped_microchip.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "context_groups" ADD COLUMN "user_id" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "context_groups" ADD COLUMN "project_id" uuid NOT NULL;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "context_groups" ADD CONSTRAINT "context_groups_project_id_projects_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."projects"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
